d083a6c4723e4ac63e16fc7e37a97f1282b2cac1
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-03-10 21:07:12 +0300

    update mode of webpack.config.js

--- git example/webpack.config.js
+++ git example/webpack.config.js
@@ -3,7 +3,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./index.js",
     output: {
         path: path.resolve(__dirname, "dev"),
